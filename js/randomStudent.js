9012b08b3c91d343917ba1ac603204b0f684e636
chipset1test <stephenaemblem@gmail.com>
2014-09-12 21:20:58 -0400

    adding to table

--- js/randomStudent.js
+++ js/randomStudent.js
@@ -1,25 +1,36 @@
-var subjects = ["math", "english", "history", "french"];
+var subjects = ["math", "english", "history", "french", "P.E"];
 var firstNames = ["John", "Jim", "Jones", "James", "Jackie", "Bob"];
-var lastName = ["Smith", "Roy", "Gagnon", "Lavoie", "Fortin", "Gagné", "Doe"];
+var lastNames = ["Smith", "Roy", "Gagnon", "Lavoie", "Fortin", "Gagne", "Doe"];
 
 function getRandomStudent(){
-	return Student;
+	// 
+	// add random subjects
+	// set courseGrades literal property
+	for(var i in subjects) {
+		var subject = subjects[i];
+		randomStudent.courseGrades[subject] = randomGrade();					
+	}
+	return randomStudent;
 }
 
-var Student = {
-	name : "ok",
+
+
+var randomStudent = {
+	name : firstNames[Math.floor(random(firstNames.length))] + " " + lastNames[Math.floor(random(firstNames.length))],
 	program: "program1",
-	level: 2,
+	level: Math.floor(random(5)),
 	//https://stackoverflow.com/questions/1208222/how-do-i-implement-a-dictionary-or-hashtable-in-javascript
-	courseGrades : {
-		math : "A",
-		english : "B-",
-		history : "C+",
-		french : "F",	
-	}
+	courseGrades : {}
 };
 
+function randomGrade(){
+	var grades = ["A", "B", "C", "D", "F"];
+	var plusOrMinus = Math.random() > 0.5 ? "-" : "+";
+	var grade = grades[Math.floor(random(grades.length))] + " " + plusOrMinus;
+	return grade;
+}
+
 function random(max){
-	//firstNames[Math.floor(random(firstNames.length))] + " " + lastNames[Math.floor(random(firstNames.length))]
+	//
 	return Math.random() * max;
 }
