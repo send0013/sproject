5c5fb14ec09729eef15dd0bf840578e0b7708cbb
chipset1test <stephenaemblem@gmail.com>
2014-09-12 21:51:19 -0400

    css table styling

--- js/randomStudent.js
+++ js/randomStudent.js
@@ -1,4 +1,4 @@
-var subjects = ["math", "english", "history", "french", "P.E"];
+var subjects = ["Math", "English", "History", "French", "P.E"];
 var firstNames = ["John", "Jim", "Jones", "James", "Jackie", "Bob"];
 var lastNames = ["Smith", "Roy", "Gagnon", "Lavoie", "Fortin", "Gagne", "Doe"];
 
@@ -6,6 +6,7 @@ function getRandomStudent(){
 	// 
 	// add random subjects
 	// set courseGrades literal property
+	var randomStudent = new RandomStudent();
 	for(var i in subjects) {
 		var subject = subjects[i];
 		randomStudent.courseGrades[subject] = randomGrade();					
@@ -13,8 +14,15 @@ function getRandomStudent(){
 	return randomStudent;
 }
 
+function RandomStudent(){
+	this.Name = firstNames[Math.floor(random(firstNames.length))] + " " + lastNames[Math.floor(random(firstNames.length))],
+	this.Program = "program1",
+	this.Level= Math.floor(random(5)),
+	//https://stackoverflow.com/questions/1208222/how-do-i-implement-a-dictionary-or-hashtable-in-javascript
+	this.courseGrades = {}   
+}
 
-
+// can't use new for literals
 var randomStudent = {
 	name : firstNames[Math.floor(random(firstNames.length))] + " " + lastNames[Math.floor(random(firstNames.length))],
 	program: "program1",
